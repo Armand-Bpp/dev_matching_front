dd6f13cd2465c0afd9286da0821d048161a0bca8
xston <toshitox@gmail.com>
2020-02-11 14:28:53 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">
+                        <img src="" width="30" height="30" alt="" />
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="/faq">FAQ <span class="sr-only">(current)</span></a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+
+}
+
+export default Navbar;
