c0df0679765d019fc64a3c02d53f957131230a57
xston <toshitox@gmail.com>
2020-02-11 14:43:07 +0100

    second push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,7 @@ class Navbar extends React.Component {
         return(
             <div>
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">
-                        <img src="" width="30" height="30" alt="" />
-                    </a>
+                    <img src="../logo.png" width="30" height="30" alt="" />
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
