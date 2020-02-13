fad0b97d15a44797114950b006ce023ab48d7151
aziza94 <50337632+aziza94@users.noreply.github.com>
2020-02-13 10:51:53 +0100

    recuprer la version corriger de abdallah

---
+++ src/components/pages/Favoris.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '../entreprise/Card'
+
+
+
+class Favoris extends React.Component {
+    render(){
+        return(
+            <div>
+                salut
+            </div>
+        )
+    }
+}
+
+
+export default Favoris;
